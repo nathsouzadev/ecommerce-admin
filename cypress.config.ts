3eb514bd5ef503d13787ccf77f1a602c1910d5a6
Nathally Souza <nathallyccd@gmail.com>
2024-04-19 22:38:32 -0300

    [CHORE]update actions

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   e2e: {
     fixturesFolder: 'fixtures',
     setupNodeEvents(on, config) {},
-    baseUrl: `http://localhost:${process.env.PORT ?? '3000'}`,
+    baseUrl: Cypress.env('NEXT_PUBLIC_APP_URL'),
   },
 });
