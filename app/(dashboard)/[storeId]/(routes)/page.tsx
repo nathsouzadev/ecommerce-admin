02734ed1787469ffd4e400922f2281f690e2085c
Nathally Souza <nathallyccd@gmail.com>
2024-04-27 10:48:16 -0300

    [feat]get store data to dashboard page

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,9 +1,37 @@
-const DashboardPage = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
+'use client'
+
+import { useEffect, useState } from "react";
+
+interface DashboardPageProps {
+  params: {
+    storeId: string;
+  }
+}
+
+const DashboardPage: React.FC<DashboardPageProps> = ({ params }) => {
+  const [store, setStore] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    fetch(`/api/store/${params.storeId}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setStore(data.store);
+        setIsLoading(false);
+      });
+  }, [params.storeId]);
+
+  if(isLoading) return <div>Loading...</div>
+  
+  if(store){
+    return (
+      <div>
+        Dashboard
+        Store: {store.name}
+      </div>
+    )
+  }
 };
 
 export default DashboardPage;
